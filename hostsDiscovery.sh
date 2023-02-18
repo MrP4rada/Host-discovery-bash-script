c7dda3210dde37253c8dc3712d5f8db5e3a94872
MrP4rada <125773507+MrP4rada@users.noreply.github.com>
2023-02-18 23:49:08 +0000

    Add files via upload

---
+++ hostsDiscovery.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+function Ctrl-c(){
+echo -e "\n\n[+] Exiting...\n"
+exit 1
+}
+#Ctrl-c
+
+trap Ctrl-c INT
+
+thirdip=$(ip addr | grep wlp2s0 | tail -n 1 | awk '{print$2}' | tr "/" " " | awk '{print$1}'| tr "." " "| awk '{print$3}')
+
+for i in $(seq 1 255);do
+
+echo -e "\n[+]This host is active -> $( fping -t10 192.168.$thirdip.$i | grep alive | awk '{print$1}' )" | grep "192" 
+
+done
